1b04f52d3623b29cbc7b47fb6ff884c4be1b2ef2
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2023-04-12 18:12:32 +0200

    Update: Version 1.0

--- app/src/main/java/com/example/testpoliceapp/SettingsNumberActivity.kt
+++ app/src/main/java/com/example/testpoliceapp/SettingsNumberActivity.kt
@@ -1,11 +1,10 @@
 package com.example.testpoliceapp
 
-import android.content.Context
 import android.os.Bundle
+import android.text.InputType
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceManager
 
 class SettingsNumberActivity : AppCompatActivity() {
 
@@ -27,7 +26,23 @@ class SettingsNumberActivity : AppCompatActivity() {
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            //NUMBER
+            val numberPreference1: EditTextPreference? = findPreference("number1")
+            val numberPreference2: EditTextPreference? = findPreference("number2")
+            val numberPreference3: EditTextPreference? = findPreference("number3")
+            //SET ONLY NUM VALUE
+            typeOf<EditTextPreference>(numberPreference1)
+            typeOf<EditTextPreference>(numberPreference2)
+            typeOf<EditTextPreference>(numberPreference3)
 
         }
+        //FUN FOR NUM ONLY
+        private fun <T> typeOf(number: EditTextPreference?) {
+            number?.setOnBindEditTextListener { editText ->
+                editText.inputType = InputType.TYPE_CLASS_NUMBER
+            }
+        }
     }
+
+
 }
